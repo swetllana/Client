b0c4ec0f939eaced9650fdf6e0420f8897a38f2b
Svetlana <svetli.0805@gmail.com>
2020-11-24 21:43:59 +0200

    Client code

---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace Client
+{
+    partial class frmClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.lblTown = new System.Windows.Forms.Label();
+            this.lblPostalCode = new System.Windows.Forms.Label();
+            this.lblProgramVersion = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblMusic = new System.Windows.Forms.Label();
+            this.lblSinger = new System.Windows.Forms.Label();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.lblHour = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtSinger = new System.Windows.Forms.TextBox();
+            this.txtMusic = new System.Windows.Forms.TextBox();
+            this.txtProgramVersion = new System.Windows.Forms.TextBox();
+            this.txtPostalCode = new System.Windows.Forms.TextBox();
+            this.txtTown = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.txtHour = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(20, 30);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Location = new System.Drawing.Point(20, 60);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(49, 13);
+            this.lblSurname.TabIndex = 1;
+            this.lblSurname.Text = "Surname";
+            // 
+            // lblTown
+            // 
+            this.lblTown.AutoSize = true;
+            this.lblTown.Location = new System.Drawing.Point(20, 90);
+            this.lblTown.Name = "lblTown";
+            this.lblTown.Size = new System.Drawing.Size(34, 13);
+            this.lblTown.TabIndex = 2;
+            this.lblTown.Text = "Town";
+            // 
+            // lblPostalCode
+            // 
+            this.lblPostalCode.AutoSize = true;
+            this.lblPostalCode.Location = new System.Drawing.Point(20, 120);
+            this.lblPostalCode.Name = "lblPostalCode";
+            this.lblPostalCode.Size = new System.Drawing.Size(63, 13);
+            this.lblPostalCode.TabIndex = 3;
+            this.lblPostalCode.Text = "Postal code";
+            // 
+            // lblProgramVersion
+            // 
+            this.lblProgramVersion.AutoSize = true;
+            this.lblProgramVersion.Location = new System.Drawing.Point(20, 150);
+            this.lblProgramVersion.Name = "lblProgramVersion";
+            this.lblProgramVersion.Size = new System.Drawing.Size(83, 13);
+            this.lblProgramVersion.TabIndex = 4;
+            this.lblProgramVersion.Text = "Program version";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(20, 180);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 5;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblMusic
+            // 
+            this.lblMusic.AutoSize = true;
+            this.lblMusic.Location = new System.Drawing.Point(20, 210);
+            this.lblMusic.Name = "lblMusic";
+            this.lblMusic.Size = new System.Drawing.Size(35, 13);
+            this.lblMusic.TabIndex = 6;
+            this.lblMusic.Text = "Music";
+            // 
+            // lblSinger
+            // 
+            this.lblSinger.AutoSize = true;
+            this.lblSinger.Location = new System.Drawing.Point(20, 240);
+            this.lblSinger.Name = "lblSinger";
+            this.lblSinger.Size = new System.Drawing.Size(37, 13);
+            this.lblSinger.TabIndex = 7;
+            this.lblSinger.Text = "Singer";
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Location = new System.Drawing.Point(20, 270);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(29, 13);
+            this.lblYear.TabIndex = 8;
+            this.lblYear.Text = "Year";
+            // 
+            // lblHour
+            // 
+            this.lblHour.AutoSize = true;
+            this.lblHour.Location = new System.Drawing.Point(20, 300);
+            this.lblHour.Name = "lblHour";
+            this.lblHour.Size = new System.Drawing.Size(30, 13);
+            this.lblHour.TabIndex = 9;
+            this.lblHour.Text = "Hour";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(116, 30);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(166, 20);
+            this.txtName.TabIndex = 10;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(116, 173);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(166, 20);
+            this.txtEmail.TabIndex = 11;
+            // 
+            // txtSinger
+            // 
+            this.txtSinger.Location = new System.Drawing.Point(116, 233);
+            this.txtSinger.Name = "txtSinger";
+            this.txtSinger.Size = new System.Drawing.Size(166, 20);
+            this.txtSinger.TabIndex = 12;
+            // 
+            // txtMusic
+            // 
+            this.txtMusic.Location = new System.Drawing.Point(116, 203);
+            this.txtMusic.Name = "txtMusic";
+            this.txtMusic.Size = new System.Drawing.Size(166, 20);
+            this.txtMusic.TabIndex = 13;
+            // 
+            // txtProgramVersion
+            // 
+            this.txtProgramVersion.Location = new System.Drawing.Point(116, 143);
+            this.txtProgramVersion.Name = "txtProgramVersion";
+            this.txtProgramVersion.Size = new System.Drawing.Size(166, 20);
+            this.txtProgramVersion.TabIndex = 14;
+            // 
+            // txtPostalCode
+            // 
+            this.txtPostalCode.Location = new System.Drawing.Point(116, 113);
+            this.txtPostalCode.Name = "txtPostalCode";
+            this.txtPostalCode.Size = new System.Drawing.Size(166, 20);
+            this.txtPostalCode.TabIndex = 15;
+            // 
+            // txtTown
+            // 
+            this.txtTown.Location = new System.Drawing.Point(116, 83);
+            this.txtTown.Name = "txtTown";
+            this.txtTown.Size = new System.Drawing.Size(166, 20);
+            this.txtTown.TabIndex = 16;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(116, 57);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(166, 20);
+            this.txtSurname.TabIndex = 17;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(116, 263);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(166, 20);
+            this.txtYear.TabIndex = 18;
+            // 
+            // txtHour
+            // 
+            this.txtHour.Location = new System.Drawing.Point(116, 293);
+            this.txtHour.Name = "txtHour";
+            this.txtHour.Size = new System.Drawing.Size(166, 20);
+            this.txtHour.TabIndex = 19;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(202, 341);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(80, 30);
+            this.btnSend.TabIndex = 20;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(333, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "label1";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(116, 341);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 30);
+            this.btnConnect.TabIndex = 22;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(294, 341);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(74, 30);
+            this.btnDisconnect.TabIndex = 23;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // frmClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 404);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtHour);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtTown);
+            this.Controls.Add(this.txtPostalCode);
+            this.Controls.Add(this.txtProgramVersion);
+            this.Controls.Add(this.txtMusic);
+            this.Controls.Add(this.txtSinger);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblHour);
+            this.Controls.Add(this.lblYear);
+            this.Controls.Add(this.lblSinger);
+            this.Controls.Add(this.lblMusic);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblProgramVersion);
+            this.Controls.Add(this.lblPostalCode);
+            this.Controls.Add(this.lblTown);
+            this.Controls.Add(this.lblSurname);
+            this.Controls.Add(this.lblName);
+            this.Name = "frmClient";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.frmClient_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.Label lblTown;
+        private System.Windows.Forms.Label lblPostalCode;
+        private System.Windows.Forms.Label lblProgramVersion;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblMusic;
+        private System.Windows.Forms.Label lblSinger;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.Label lblHour;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtSinger;
+        private System.Windows.Forms.TextBox txtMusic;
+        private System.Windows.Forms.TextBox txtProgramVersion;
+        private System.Windows.Forms.TextBox txtPostalCode;
+        private System.Windows.Forms.TextBox txtTown;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.TextBox txtHour;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+    }
+}
+
---
+++ Client/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Client
+{
+    public partial class frmClient : Form
+    {
+        string address = "127.0.0.1";
+        // SSL server port
+        int port = 2222;
+        // Create and prepare a new SSL client context
+        SslContext context = new SslContext();
+        // Create a new SSL chat client
+        ChatClient client = new ChatClient();
+
+        public frmClient()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            context = new SslContext(SslProtocols.Tls12, new X509Certificate2("client.pfx", "qwerty"), (senderr, certificate, chain, sslPolicyErrors) => true);
+            client = new ChatClient(context, address, port);
+
+            // Connect the client
+            label1.Text = "Client connecting...";
+            client.ConnectAsync();
+            label1.Text = "Done!";
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            Data data = new Data();
+            data.Name = txtName.Text;
+            data.Surname = txtSurname.Text;
+            data.Town = txtTown.Text;
+            data.PostalCode = txtPostalCode.Text;
+            data.ProgramVersion = txtProgramVersion.Text;
+            data.Email = txtEmail.Text;
+            data.Music = txtMusic.Text;
+            data.Singer = txtSinger.Text;
+            data.Year = txtYear.Text;
+            data.Hour = txtHour.Text;
+
+            //serialize info
+            string jsonString = JsonConvert.SerializeObject(data);
+
+            //gzip
+            var message = "";
+            byte[] inputBytes = Encoding.UTF8.GetBytes(jsonString);
+            using (var outputStream = new MemoryStream())
+            {
+                using (var gZipStream = new GZipStream(outputStream, CompressionMode.Compress))
+                    gZipStream.Write(inputBytes, 0, inputBytes.Length);
+                var outputBytes = outputStream.ToArray();
+                var outputbase64 = Convert.ToBase64String(outputBytes);
+                message = outputbase64;
+            }
+
+            client.SendAsync(message);
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            // Disconnect the client
+            label1.Text = "Client disconnecting...";
+            client.DisconnectAndStop();
+            label1.Text = "Done!";
+        }
+
+        private void frmClient_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
